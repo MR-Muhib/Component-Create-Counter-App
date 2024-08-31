658c4be2784aca80c82f2daeacf3a90fbee4aef6
mrmuhib300@gmail.com <mrmuhib300@gmail.com>
2024-08-31 23:35:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+/** import useState use kore amra updatedal funsition creat kore */
 
 function App() {
+  const [dynamicCount, setDynamicCount] = useState(20);
+  const [dynamicCount2, setDynamicCount2] = useState(50);
+
+  /** useState amader akti [] arry return kore jar modde amra argument paramitar use kore count ke dynamic korte parbo */
+
+  const increaseHandler = () => {
+    setDynamicCount(dynamicCount + 1);
+  };
+
+  const discrisHandler = () => {
+    setDynamicCount(dynamicCount - 1);
+  };
+
+  const increaseHandler2 = () => {
+    setDynamicCount2(dynamicCount2 + 1);
+  };
+
+  const discrisHandler2 = () => {
+    setDynamicCount2(dynamicCount2 - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <p>Dynamic Data count {dynamicCount}</p>
+        <button onClick={increaseHandler}>Increment Data</button>
+        <button onClick={discrisHandler}>Discriminant Data</button>
+      </div>
+
+      <div className="container2">
+        <p>Dynamic Data count {dynamicCount2}</p>
+        <button onClick={increaseHandler2}>Increment Data</button>
+        <button onClick={discrisHandler2}>Discriminant Data</button>
+      </div>
+    </>
   );
 }
 
